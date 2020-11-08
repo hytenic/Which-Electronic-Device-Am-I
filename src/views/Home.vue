0cf0a4a0aaf6a3423612adaf2cd1ea2555812e3c
sean_park <19980819@naver.com>
2020-11-08 21:21:45 +0900

    add home.vue style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div>나는 어떤 전자기기일까?</div>
-    <button>시작하기</button>
+    <div id="title">나는 어떤 전자기기일까?</div>
+    <button id="btn-start">시작하기</button>
     <footer>
-    <img src="../assets/logo.png" alt="logo">
+    <img class="logo-img" src="../assets/logo.png" alt="logo">
     </footer>
   </div>
 </template>
@@ -20,5 +20,24 @@ export default {
 </script>
 
 <style>
+#title {
+  font-size: 25px;
+  margin-bottom: 500px;
+  font-weight: bold;
+}
 
+#btn-start {
+  width: 65%;
+  height: 70px;
+  border-radius: 20px;
+  border: 0px;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+  background-color: #2e4f67;
+}
+
+.logo-img {
+  margin: 50px;
+}
 </style>
