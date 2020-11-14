b301f9e283731780cc3896a6487d3334e6ee6ae8
elenajel <jelee.elena@gmail.com>
2020-11-14 20:32:07 +0900

    Inro

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-  This is Intro
+    <div id="textBox">
+      <div id="textMessage">발신: 1004 - 1004 오전 6:00
+        <div>그동안 수고한 당신 오늘 하루 쉬세요 :)</div>
+      </div>
+    </div>
+    <div style="width: 50%">
+      별안간 꿀같은 휴식이 주어졌다. 오늘 하루 어떻게 보낼까?
+    </div>
   </div>
 </template>
 
@@ -14,3 +21,16 @@ export default {
   }
 }
 </script>
+
+<style>
+#textBox {
+  border:1px solid #2e4f67;
+  width: 50%;
+  height: 150px;
+}
+
+#textMessage {
+  margin-top: 10px;
+  font-size: 15px;
+}
+</style>
