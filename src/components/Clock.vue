f059965178953d343a65c3beaff416d9db004f97
elenajel <jelee.elena@gmail.com>
2020-11-21 16:51:39 +0900

    Edit intro page

---
+++ src/components/Clock.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div id="clock">
+      <div id="time">06:00</div>
+      <div id="date">{{ months }} 월 {{dates}} 일 {{days}}요일</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      months: '',
+      dates: '',
+      days: ''
+    }
+  },
+  created: function () {
+    console.log('created')
+    this.updateDateTime()
+  },
+  methods: {
+    updateDateTime: function updateDateTime () {
+      var week = ['일', '월', '화', '수', '목', '금', '토']
+      var now = new Date()
+      this.months = now.getMonth() + 1
+      this.dates = now.getDate()
+      this.days = week[now.getDay()]
+      console.log(now.getMonth())
+    }
+  }
+}
+</script>
+
+<style>
+#clock {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+#time {
+  font-size: 72px;
+  font-weight: 200;
+}
+#date {
+  font-size: 18px;
+  font-weight: 300;
+}
+</style>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,50 +1,77 @@
 <template>
   <div>
-    <div id="textBox">
-      <div id="textMessage">발신: 1004 - 1004 오전 6:00
-        <div>그동안 수고한 당신 오늘 하루 쉬세요 :)</div>
+    <img src="../assets/status_bar.png" alt="status bar" id="status-bar">
+    <Clock id="clock"/>
+    <img  id="message" src="../assets/message_light_new.png" alt="text message">
+
+    <div id="modal" v-show=checked>
+      <div id="notice">
+        !!! 갑자기 생긴 꿀같은 휴식
+        <br> 오늘 하루 어떻게 보내볼까?
+        <router-link to="/question">
+          <button id="btn-start-questions">휴식 START!</button>
+        </router-link>
       </div>
+
     </div>
-    <div style="width: 80%; margin: 0 auto">
-      별안간 꿀같은 휴식이 주어졌다. 오늘 하루 어떻게 보낼까?
-    </div>
-    <router-link to="/question">
-      <button id="btn-start">시작하기</button>
-    </router-link>
   </div>
 </template>
 
 <script>
+import Clock from '../components/Clock'
 export default {
   name: 'Intro',
   data () {
     return {
-
+      checked: true
     }
+  },
+  components: {
+    Clock
+  },
+  methods: {// on click, change img & show modal
   }
 }
 </script>
 
 <style>
-#textBox {
-  border:1px solid #2e4f67;
-  width: 80%;
-  height: 150px;
+body {
+  background-color: #787878;
+}
+#status-bar {
+  width: 25%;
+  float: right;
+  margin-top: -50px; /*app style 바꿔야하지 않을까.. */
+}
+#clock {
+  margin-top: 20%;
+  width: 100%;
+  color: #000000;
+}
+#message {
+  width: 96%;
+  margin-top: 15%;
+}
+#modal {
+  width: 94%;
+  height: 205px;
+  background-color: white;
+  border-radius: 12px;
   margin: 0 auto;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
 }
-
-#textMessage {
-  margin-top: 10px;
-  font-size: 15px;
+#notice {
+  font-size: 20px;
+  color: #000000;
 }
-#btn-start {
-  width: 65%;
-  height: 70px;
-  border-radius: 20px;
+#btn-start-questions {
+  margin-top: 10%;
   border: 0px;
-  color: white;
-  font-size: 30px;
+  color: #3DADFF;
+  font-size: 22px;
   font-weight: bold;
-  background-color: #2e4f67;
+  background-color: transparent;
 }
 </style>
