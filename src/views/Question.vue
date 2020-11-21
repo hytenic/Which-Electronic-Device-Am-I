4c4a9b0246178fc4a4f17a2a887d1bb0d849621d
Robin Kim <robin@gomicorp.com>
2020-11-21 19:44:06 +0900

    ADD: questions with data!

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,29 +1,63 @@
 <template>
   <div>
-  This is Question
-    <button @click="onClickBtn">
-    버튼입니다
-    </button>
-  <div>{{progress}}</div>
+    <div class="title">{{question}}</div>
+    <div v-for="answer in answers" :key="answer._id" >
+      <button class="btn-answer" @click="onClickBtn">
+        {{answer}}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import questions from '../data/Questions.json'
 export default {
   name: 'Question',
   data () {
     return {
-      progress: 0
+      progress: 1,
+      totalQuestionCount: 8,
+      questions: questions
     }
   },
   methods: {
     onClickBtn () {
-      if (this.progress > 10) {
+      if (this.progress++ >= this.totalQuestionCount) {
         this.$router.push('/result')
       }
-      this.progress++
-      console.log(this.progress)
+    }
+  },
+  computed: {
+    question: function () {
+      return this.questions[this.progress - 1].question
+    },
+    answers: function () {
+      return this.questions[this.progress - 1].answers
     }
   }
 }
 </script>
+
+<style>
+.title {
+  font-size: 25px;
+  font-weight: bold;
+  margin-bottom: 30px;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.btn-answer {
+  width: 80%;
+  height: 70px;
+  border-radius: 20px;
+  border: 0px;
+  color: white;
+  font-size: 20px;
+  font-weight: normal;
+  padding: 10px;
+  background-color: #2e4f67;
+  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.25);
+  margin: 5px;
+}
+</style>
