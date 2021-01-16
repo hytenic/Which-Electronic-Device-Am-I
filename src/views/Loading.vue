44047839329baa64ddf0c7d5a68883cd4784dd98
sean_park <19980819@naver.com>
2021-01-16 19:34:17 +0900

    add : initial commit

---
+++ src/views/Loading.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <img class="sun-img" src="@/assets/sun2.png" />
+    <div id="loading-msg">
+      두구두구두구...!!!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  },
+  mounted: function () {
+    const route = () => { this.$router.push('/result') }
+    setTimeout(route, 2000)
+  }
+}
+</script>
+
+<style>
+
+#loading-msg {
+  margin: 100% auto 50% auto;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.sun-img {
+  width: 70px;
+  left: 40%;
+  top: 30%;
+  position: absolute;
+}
+
+</style>
